dd961518ff225b69d9d2be394bbcf120b86391a4
Mauricio Rojas <mrojasb2000@gmail.com>
2021-01-30 17:45:40 -0300

    Refactor create users base code

--- services/index.services.ts
+++ services/index.services.ts
@@ -6,35 +6,40 @@ interface User {
 }
 
 const users: User[] = [{
-    id: v4.generate(),
-    name: "Ryan Ray",
+  id: v4.generate(),
+  name: "Ryan Ray",
 }];
 
 export const getUsers = ({ response }: { response: Response }) => {
   response.body = {
     message: "successful query",
-    users,
+    data: users,
   };
 };
 
-interface userBody {
-    name: string,
-}
 export const createUsers = async (
   { request, response }: { request: Request; response: Response },
 ) => {
-    /* const { id, name } = await request.body().value
-    console.log(name) */
+  
+    if (!request.hasBody) {
+      response.status = 400;
+      response.body = {
+        message: "No data provided",
+      };
+      return;
+    }
 
     const  { name } = await request.body().value
-    
     users.push({
         id: v4.generate(),
         name: name
     })
+
     response.body = {
-        message: "received"
+        message: "user created",
+        data: users.slice(-1)[0]
     }
+    response.status = 201
 };
 
 /* export const getUser = () => {}
