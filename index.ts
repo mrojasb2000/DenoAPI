e2a3388536a7ffb280140e235382ab6f0bc54ad3
Mauricio Rojas <mrojasb2000@gmail.com>
2021-01-30 15:26:03 -0300

    Added routes structure on folder routes

--- index.ts
+++ index.ts
@@ -1,15 +1,10 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import router from './routes/index.routes.ts'
 
 const app = new Application();
-
-const router = new Router();
-
-router.get("/", (ctx) => {
-  ctx.response.body = "Hello World!";
-})
 
 app.use(router.routes());
 
 const port = 8000
-console.log("Server running on port: " + port)
+console.log("Server running on port: ", port)
 await app.listen({ port: port });
